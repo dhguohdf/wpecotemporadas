ab319f586f258d7e80d049e561ac9e382311ebc3
unknown <matiaseveraldo@gmail.com>
2013-12-17 18:07:59 -0200

    Melhorando funcao de upload de imagens no Listing.

--- wp-content/themes/shandora1.0/novo-mundo/funcoes.php
+++ wp-content/themes/shandora1.0/novo-mundo/funcoes.php
@@ -23,7 +23,7 @@ function conteudo_editor( $content, $post ) {
 
 
 /**
- * Trocando mensagem ao salvar o {listing}.
+ * Trocando mensagem ao salvar o {post}.
  */
 function admin_messages($messages) {
 	global $post, $post_ID;
@@ -47,30 +47,34 @@ function admin_messages($messages) {
 
 add_filter('post_updated_messages', 'admin_messages' );
 
-add_filter('wp_handle_upload_prefilter', 'limite_upload');
-function limite_upload( $file ) {
+
+/**
+ * Adicionando limite de uploads no post type listing
+ */
+function limite_upload($file) {
 	global $post, $post_ID;
 	$post_type = get_post_type( $post_ID );
-	if ( $post_type == 'post' ) {
 	
-	  if ($file['type']=='application/octet-stream' && isset($file['tmp_name'])) {
-		$file_size = getimagesize($file['tmp_name']);
-		if (isset($file_size['error']) && $file_size['error']!=0) {
-		  $file['error'] = "Unexpected Error: {$file_size['error']}";
-		  return $file;
-		} else {
-		  $file['type'] = $file_size['mime'];
+  if ($file['type']=='application/octet-stream' && isset($file['tmp_name'])) {
+    $file_size = getimagesize($file['tmp_name']);
+    if (isset($file_size['error']) && $file_size['error']!=0) {
+      $file['error'] = "Unexpected Error: {$file_size['error']}";
+      return $file;
+    } else {
+      $file['type'] = $file_size['mime'];
+    }
+  }
+  list($category,$type) = explode('/',$file['type']);
+
+  if ('image'!=$category || !in_array($type,array('jpg','jpeg','gif','png')) && $post_type == "listing" ) {
+    $file['error'] = "Sorry, you can only upload a .GIF, a .JPG, or a .PNG image file.";
+  } else if ($post_id = (isset($_REQUEST['post_id']) ? $_REQUEST['post_id'] : false)) {
+
+		if ( count( get_posts( "post_type=attachment&post_parent={$post_id}" ) ) > 2 )
+			$file['error'] = "Você atingiu seu limite de imagens (3).";
 		}
-	  }
-	  list($category,$type) = explode('/',$file['type']);
-	  if ('image'!=$category || !in_array($type,array('jpg','jpeg','gif','png'))) {
-		$file['error'] = "Sorry, you can only upload a .GIF, a .JPG, or a .PNG image file.";
-	  } else if ($post_id = (isset($_REQUEST['post_id']) ? $_REQUEST['post_id'] : false)) {
-		if (count(get_posts("post_type=attachment&post_parent={$post_id}"))>2)
-		  $file['error'] = "Sorry, you cannot upload more than one (3) image.";
-	  }
-	} 
   return $file;
 }
 
+add_filter('wp_handle_upload_prefilter', 'limite_upload(');
 ?>
